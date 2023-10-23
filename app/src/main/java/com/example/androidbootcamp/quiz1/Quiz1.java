178b797a80918817255bf71cbd39aa26858db469
Ali <alibyslen@gmail.com>
2023-10-24 02:13:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidbootcamp/quiz1/Quiz1.java
@@ -0,0 +1,205 @@
+package com.example.androidbootcamp.quiz1;
+
+public class Quiz1 {
+
+    public static void main(String[] args) {
+
+
+        // İlçe
+        String country = "Bağcılar";
+
+        // Kıta
+        String continent = "Avrupa";
+
+        // Faks Numarası
+        String fax = "+44-161-999-8888";
+
+        // Posta Kodu
+        String postCode = "00034";
+
+        // İnstagram Adresi
+        String instaAddress = "ali_bysln";
+
+        // Çalıştığın Bölüm
+        String departmentYouWorkIn = "Developer";
+
+        // Ürün Miktarı
+        int productQuantity = 23;
+
+        // Müşteri Soyadı
+        String customerLastname = "Beysülen";
+
+        // Ödeme Miktarı
+        int paymentAmount = 1203004005;
+
+        // Doğum Tarihi
+        String dateOfBirth = "06/07/2001";
+
+        // Borç
+        int debt = 3500;
+
+        // Medeni Hal
+        String maritalStatus = "Bekar";
+
+        // Video Yorumu
+        String videoComment = "Güzel fakat biraz daha uzun olabilirdi.";
+
+        // Ödeme Saati
+        String paymentTime = "13:25";
+
+        // EFT Miktarı
+        double eftAmount = 0.50;
+
+        // Satılan Miktar
+        int amountSold = 34;
+
+        // Telefon Modeli
+        String phoneModel = "Samsung Galaxy S20+";
+
+        // Dergi Adı
+        String journalName = "Science";
+
+        // Basım Tarihi
+        String printingDate = "24/10/2023";
+
+        // Zam Miktarı
+        double raiseAmount = 1685.50;
+
+        // Daire Sayısı
+        int numberOfApartments = 7;
+
+        // Enlem
+        double latitude = 41.000;
+
+        // Boylam
+        double longitude = 28.000;
+
+        // Yemek Adı
+        String foodName = "İçli Köfte";
+
+        // Ürün Fiyatı
+        int productPrice = 6523;
+
+        // Şirket
+        String company = "Techcareer";
+
+        // Video Adı
+        String videoName = "Java ile Mobil Uygulama Geliştirme";
+
+        // Müzik Süresi
+        double musicTime = 3.45;
+
+        // Mekan Puanı
+        int venueScore = 5;
+
+        // Dosya Adı
+        String fileName = "AndroidProjects";
+
+        // Resim Formatı
+        String imageFormat = "image.png";
+
+        // Renk
+        String colour = "Kırmızı";
+
+        // Renk Kodu
+        String colourCode = "#FF0000";
+
+        // Bilgisayar Modeli
+        String computerModel = "GE63 7RD Raider";
+
+        // Ekran Boyutu
+        String screenSize = "1920x1080";
+
+        // Kullanım Süresi
+        String durationOfUse = "1 saat";
+
+        // Basınç
+        String pressure = "1.01325 bar";
+
+        // Etkinlik Günü
+        String eventDay = "26/10/2023";
+
+        // Ödeme Günü
+        String paymentDay = "24/10/2023";
+
+        // Yolculuğun Çıkış Tarihi
+        String departureDateOfTheJourney = "10/11/2023";
+
+        // Mahalle Adı
+        String neighborhoodName = "Yeni Mahalle";
+
+        // Otobüs Hattı
+        String busLine = "E-50";
+
+        // Kullanılan Dakika
+        int minutesUsed = 120;
+
+        // Kargo Takip Numarası
+        int cargoTrackingNumber = 123456789;
+
+        // Kupon Süresi
+        int couponDuration = 30;
+
+        // Kupon Kodu
+        int couponCode = 123456789;
+
+        // Fatura Tarihi
+        String invoiceDate = "06/07/2021";
+
+
+        // Oluşturmuş olduğumuz değişkenleri konsolda görmek için
+        // System.out.println() fonksiyonunu kullanarak içerisinde açıklama ile beraber değişkenin adını + operatöründen sonra yazıyoruz
+
+        System.out.println("İlçe : " + country);
+        System.out.println("Kıta : " + continent);
+        System.out.println("Faks Numarası : " + fax);
+        System.out.println("Posta Kodu : " + postCode);
+        System.out.println("İnstagram Adresi : " + instaAddress);
+        System.out.println("Çalıştığın Bölüm : " + departmentYouWorkIn);
+        System.out.println("Ürün Miktarı : " + productQuantity);
+        System.out.println("Müşteri Soyadı : " + customerLastname);
+        System.out.println("Ödeme Miktarı : " + paymentAmount);
+        System.out.println("Doğum Tarihi : " + dateOfBirth);
+        System.out.println("Borç : " + debt);
+        System.out.println("Medeni Hal : " + maritalStatus);
+        System.out.println("Video Yorumu : " + videoComment);
+        System.out.println("Ödeme Saati : " + paymentTime);
+        System.out.println("EFT Miktarı : " + eftAmount);
+        System.out.println("Satılan Miktar : " + amountSold);
+        System.out.println("Telefon Modeli : " + phoneModel);
+        System.out.println("Dergi Adı : " + journalName);
+        System.out.println("Basım Tarihi : " + printingDate);
+        System.out.println("Zam Miktarı : " + raiseAmount);
+        System.out.println("Daire Sayısı : " + numberOfApartments);
+        System.out.println("Enlem : " + latitude);
+        System.out.println("Boylam : " + longitude);
+        System.out.println("Yemek Adı : " + foodName);
+        System.out.println("Ürün Fiyatı : " + productPrice);
+        System.out.println("Şirket : " + company);
+        System.out.println("Video Adı : " + videoName);
+        System.out.println("Müzik Süresi : " + musicTime);
+        System.out.println("Mekan Puanı : " + venueScore);
+        System.out.println("Dosya Adı : " + fileName);
+        System.out.println("Resim Formatı : " + imageFormat);
+        System.out.println("Renk : " + colour);
+        System.out.println("Renk Kodu : " + colourCode);
+        System.out.println("Bilgisayar Modeli : " + computerModel);
+        System.out.println("Ekran Boyutu : " + screenSize);
+        System.out.println("Kullanım Süresi : " + durationOfUse);
+        System.out.println("Basınç : " + pressure);
+        System.out.println("Etkinlik Günü : " + eventDay);
+        System.out.println("Ödeme Günü : " + paymentDay);
+        System.out.println("Yolculuğun Çıkış Tarihi : " + departureDateOfTheJourney);
+        System.out.println("Mahalle Adı : " + neighborhoodName);
+        System.out.println("Otobüs Hattı : " + busLine);
+        System.out.println("Kullanılan Dakika : " + minutesUsed);
+        System.out.println("Kargo Takip Numarası : " + cargoTrackingNumber);
+        System.out.println("Kupon Süresi : " + couponDuration);
+        System.out.println("Kupon Kodu : " + couponCode);
+        System.out.println("Fatura Tarihi : " + invoiceDate);
+
+
+
+    }
+
+}
